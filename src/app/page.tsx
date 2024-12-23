c990adc8e7eab5f3c26602859c3488aa753ca616
hiimvikash <heyimvikash@gmail.com>
2024-12-23 11:45:34 +0530

    Basic project setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div className="text-2xl text-green-700">
+      Hello 
+      <Button>Helloooooo</Button>
+    </div>
+  );
+}
